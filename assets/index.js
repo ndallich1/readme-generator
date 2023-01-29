a839b9d27c1bd957ccc53843281999c1c700e225
ndallich1 <ndallich@gmail.com>
2023-01-29 17:53:51 -0500

    reorganized files

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer")
-const fs = require("fs")
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
